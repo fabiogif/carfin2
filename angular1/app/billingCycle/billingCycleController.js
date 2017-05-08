412bb75ec499173be4f13c8f730b0428cc796284
fabiogif <fabio@MacBook-Air-de-Usuario.local>
2017-05-07 23:08:37 -0300

    Exibição das Abas de Alterar e Excluir
    Exibição das Abas de Alterar e Excluir

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,22 +1,44 @@
 (function(){
     angular.module('pedidorapido').controller('billingCycleCtrl',[
           '$http',
+          'msgs',
+          'tabs',
           BillingCycleController
         ])
 
 
-        function BillingCycleController($http){
+        function BillingCycleController($http, msgs, tabs){
             const vm = this
+            const url = 'http://localhost:3003/api/billingCycles'
 
-            vm.create = function(){
-                const url = 'http://localhost:3003/api/billingCycles'
+            vm.refresh = function(){
+              $http.get(url).then(function(response){
+                vm.billingCycle = {}
+                vm.billingCycles = response.data
+                tabs.show(vm, {tabList: true, tabCreate: true})
+              })
+            }
+          vm.create = function(){
                 $http.post(url, vm.billingCycle).then(function(response){
-                    vm.billingCycle = {}
-                    //console.log('Sucesso')
+                    vm.refresh()
+                      //vm.billingCycles = response
+                    msgs.addSuccess('Operação realizada com sucesso')
 
+                }).catch(function(response){
+                  msgs.addError(response.data.errors)
                 })
 
             }
+            vm.showTabUpdate = function(billingCycle){
+              vm.billingCycle = billingCycle
+              tabs.show(vm, {tabUpdate: true})
+            }
+            vm.showTabDelete = function(billingCycle){
+              vm.billingCycle = billingCycle
+              tabs.show(vm, {tabDelete: true})
+            }
+
+            vm.refresh()
 
         }
 
